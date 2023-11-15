1e33367916d345089baf83492e0651df9ffbd049
IcterusGames <112021348+IcterusGames@users.noreply.github.com>
2023-11-15 17:11:20 -0400

    Version 1.0.0

--- scenes/showroom.gd
+++ scenes/showroom.gd
@@ -7,7 +7,6 @@ func _ready():
 	Globals.scene_config_changed.connect(apply_config)
 	await get_tree().process_frame
 	apply_config()
-	#get_viewport().debug_draw = Viewport.DEBUG_DRAW_SSAO
 
 
 func _process(delta):
@@ -16,6 +15,37 @@ func _process(delta):
 		$CamAxisY/CamAxisX.rotate_x(Input.get_axis(&"ui_up", &"ui_down") * delta * 0.25)
 	if _cam_auto_rotate:
 		$CamAxisY.rotate_y(delta * 0.1)
+
+
+func _unhandled_key_input(event : InputEvent):
+	if event.pressed and event.keycode == KEY_1:
+		$CamAxisY.rotation.y = deg_to_rad(39)
+		$CamAxisY/CamAxisX.rotation.x = deg_to_rad(-10.8)
+		_cam_auto_rotate = false
+	if event.pressed and event.keycode == KEY_2:
+		$CamAxisY.rotation.y = deg_to_rad(130)
+		$CamAxisY/CamAxisX.rotation.x = deg_to_rad(-10.8)
+		_cam_auto_rotate = false
+	if event.pressed and event.keycode == KEY_3:
+		$CamAxisY.rotation.y = deg_to_rad(220)
+		$CamAxisY/CamAxisX.rotation.x = deg_to_rad(-10.8)
+		_cam_auto_rotate = false
+	if event.pressed and event.keycode == KEY_4:
+		$CamAxisY.rotation.y = deg_to_rad(330)
+		$CamAxisY/CamAxisX.rotation.x = deg_to_rad(-10.8)
+		_cam_auto_rotate = false
+	if event.pressed and event.keycode == KEY_5:
+		$CamAxisY.rotation.y = deg_to_rad(22.23)
+		$CamAxisY/CamAxisX.rotation.x = deg_to_rad(-4.47)
+		_cam_auto_rotate = false
+	#if event.pressed and event.keycode == KEY_6:
+		#$CamAxisY.rotation.y = deg_to_rad(180.0)
+		#$CamAxisY/CamAxisX.rotation.x = deg_to_rad(-4.47)
+		#$CamAxisY/CamAxisX/Camera3D.position.z = 0
+		#$CamAxisY.position.x = 0.4
+		#$CamAxisY.position.y = 1.05
+		#$CamAxisY.position.z = 0.0
+		#_cam_auto_rotate = false
 
 
 func _input(event):
